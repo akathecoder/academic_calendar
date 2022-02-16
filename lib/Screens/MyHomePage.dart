0a962c3e4bd85ce451efb73265a9e8767f312b54
Sparsh Agarwal <sparshaga25@gmail.com>
2022-02-16 13:39:09 +0530

    cleaned src folder

---
+++ lib/Screens/MyHomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
